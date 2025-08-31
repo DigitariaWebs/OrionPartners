a0e47eaba2c45a2865fd0a7905cf16822d53d6da
mohameddn988 <msideneche@gmail.com>
2025-08-31 19:11:35 +0100

    refactor: remove unused Link import from ServiceModel component

--- src/components/models/ServiceModel.tsx
+++ src/components/models/ServiceModel.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { X, Check } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useModal } from "../providers/ModalProvider";
 
